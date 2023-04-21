f09f923c6286f230660abe64985d4a3ad1d2568a
mediokrity <kritydangol1@gmail.com>
2023-04-21 13:06:49 +0545

    Dynamic NAV done

--- menu/menu.js
+++ menu/menu.js
@@ -1,15 +1,39 @@
+// Rendering NAV
+const navEl = document.getElementById("nav")
+
+function navRender() {
+    let navLit = `
+    <a href="/mains" class="logo">
+        <img src="../images/logo-short.png" alt="Logo">
+        <h2>Krity Dangol</h2>
+        <p>Front-end Developer</p>
+    </a>
+
+    <label>
+        <input type="checkbox">
+        <span class="menu"> <span class="hamburger"></span> </span>
+        <ul id="nav-links"> </ul>
+    </label>
+    `
+    navEl.innerHTML = navLit
+}
+
+navRender()
+
+
+// Rendering nav MENU
 const navMenu = document.getElementById("nav-links")
 
 const menuItems = [
     { link: "/", item: "Home" },
     { link: "/mains", item: "Mains" },
 ]
-
 function renderMenu() {
-    navDOM = ""
+    let navDOM = ""
     for (let i = 0; i < menuItems.length; i++) {
         navDOM += `<li> <a href="${menuItems[i].link}">${menuItems[i].item}</a> </li>`
         navMenu.innerHTML = navDOM
     }
 }
+
 renderMenu()
