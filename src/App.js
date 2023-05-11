a356642464940582a4e9cb8be257fb78cffa5d8a
mediokrity <kritydangol1@gmail.com>
2023-05-11 13:14:55 +0545

    updates for home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import logo from './assets/images/logo-short.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+        <img src={logo} alt="logo" class="logo-img"/>
+        <h1>Under Construction.</h1>
+    </section>
   );
 }
 
