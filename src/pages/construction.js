3bf1ce381c1bbb1b5f3944f860429b3459060de4
mediokrity <kritydangol1@gmail.com>
2023-05-12 13:28:20 +0545

    styles updated

--- src/pages/construction.js
+++ src/pages/construction.js
@@ -3,7 +3,7 @@ import logo from '../assets/images/logo-short.png';
 
 const Construction = () => {
     return (
-        <section>
+        <section className='construction'>
             <img src={logo} alt="logo" class="logo-img" />
             <h1>Under Construction.</h1>
         </section>
