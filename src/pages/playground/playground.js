019ffde8a355685618a5b78bd7bc10b685c11ae6
mediokrity <kritydangol1@gmail.com>
2023-06-21 19:59:20 +0545

    added playground

---
+++ src/pages/playground/playground.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./playground.css";
+
+const Playground = () => {
+  return <div>Playground</div>;
+};
+
+export default Playground;
--- src/utils/routing.js
+++ src/utils/routing.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 
-// importing 
+// importing
 import Const from "../pages/construction";
 import Home from "../pages/home";
+import Playground from "../pages/playground/playground";
 // import Nav from "../components/nav";
 
 const Routing = () => {
@@ -12,12 +13,13 @@ const Routing = () => {
       <Routes>
         <Route exact path="/" element={<Const />} />
         <Route exact path="/home" element={<Home />} />
+        <Route exact path="/playground" element={<Playground />} />
 
         {/* default routes for invalid links */}
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default Routing
+export default Routing;
