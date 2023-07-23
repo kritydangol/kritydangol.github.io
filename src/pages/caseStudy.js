ecb224f6789ae43d0553433c0c07e7e71d77077d
mediokrity <kritydangol1@gmail.com>
2023-07-23 22:12:03 +0545

    content update

--- src/pages/caseStudy.js
+++ src/pages/caseStudy.js
@@ -3,7 +3,22 @@ import React from "react";
 const CaseStudy = () => {
   return (
     <div>
-      <h1>CaseStudy</h1>
+      <h5>Case study</h5>
+      <h1>PSV — responsive website development</h1>
+      <h5>My role</h5>
+      <h4 class="header-dark">
+        Creative Direction
+        <br />
+        UI / UX
+        <br />
+        Website Development
+        <br />
+        Deployment
+      </h4>
+      <h5>Client</h5>
+      <h4 class="header-dark">Triniso</h4>
+      <h5>Year</h5>
+      <h4 class="header-dark">2017</h4>
     </div>
   );
 };
