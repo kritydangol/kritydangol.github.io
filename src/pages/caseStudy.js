0b4041c37cf2eb42d963c6f169551b5f7d312519
mediokrity <kritydangol1@gmail.com>
2023-07-24 18:03:54 +0545

    resolved babel error: dependencies

--- src/pages/caseStudy.js
+++ src/pages/caseStudy.js
@@ -3,22 +3,27 @@ import React from "react";
 const CaseStudy = () => {
   return (
     <div>
-      <h5>Case study</h5>
-      <h1>PSV — responsive website development</h1>
-      <h5>My role</h5>
-      <h4>
-        Creative Direction
-        <br />
-        UI / UX
-        <br />
-        Website Development
-        <br />
-        Deployment
-      </h4>
-      <h5>Client</h5>
-      <h4>Triniso</h4>
-      <h5>Year</h5>
-      <h4>2017</h4>
+      <div>
+        <h5>Case study</h5>
+        <h1>PSV — responsive website development</h1>
+        <h5>My role</h5>
+        <h4>
+          Creative Direction
+          <br />
+          UI / UX
+          <br />
+          Website Development
+          <br />
+          Deployment
+        </h4>
+        <h5>Client</h5>
+        <h4>Parth Student Venture</h4>
+        <h5>Year</h5>
+        <h4>2017</h4>
+      </div>
+      <div className="images">
+        <img src={require("../assets/images/PSV mockups.jpg")} alt="" />
+      </div>
     </div>
   );
 };
