58b1916fac606ee04b2a81a663952669158c8ad6
mediokrity <kritydangol1@gmail.com>
2023-07-24 18:09:15 +0545

    card started

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,7 @@ const Home = () => {
       <section className="lg:mx-56 sm:mx-10 mx-5">
         <p className="subH">Selected Projects</p>
         <h3 className="midH">Case Studies</h3>
+        <div className="w-[50%] h-[70vh] bg-neutral-500 rounded-md"></div>
       </section>
     </>
   );
