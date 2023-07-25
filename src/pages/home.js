2f63e4dd13a0ff7cd7e42a575d45c9e5501ce51b
mediokrity <kritydangol1@gmail.com>
2023-07-25 20:43:50 +0545

    link added

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,7 +9,9 @@ const Home = () => {
       <section className="lg:mx-56 sm:mx-10 mx-5">
         <p className="subH">Selected Projects</p>
         <h3 className="midH">Case Studies</h3>
-        <div className="w-[50%] h-[70vh] bg-neutral-500 rounded-md"></div>
+        <a href="/caseStudy">
+          <div className="w-[50%] h-[70vh] bg-neutral-500 rounded-md"></div>
+        </a>
       </section>
     </>
   );
