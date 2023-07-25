84eba6f6e94c3da05811a20316df3e89dfc8a475
mediokrity <kritydangol1@gmail.com>
2023-07-25 22:13:18 +0545

    content added

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,7 +10,10 @@ const Home = () => {
         <p className="subH">Selected Projects</p>
         <h3 className="midH">Case Studies</h3>
         <a href="/caseStudy">
-          <div className="w-[50%] h-[70vh] bg-neutral-500 rounded-md"></div>
+          <div className="w-[50%] h-[70vh] bg-neutral-500 rounded-md p-7">
+            <h5>Case study</h5>
+            <h1>PSV — responsive website development</h1>
+          </div>
         </a>
       </section>
     </>
