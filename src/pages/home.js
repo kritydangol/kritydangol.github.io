c402aaac19e909707e8fab71d62affd85560a7f2
mediokrity <kritydangol1@gmail.com>
2023-07-26 18:22:08 +0545

    home updated card

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,12 +9,24 @@ const Home = () => {
       <section className="lg:mx-56 sm:mx-10 mx-5">
         <p className="subH">Selected Projects</p>
         <h3 className="midH">Case Studies</h3>
-        <a href="/caseStudy">
-          <div className="w-[50%] h-[70vh] bg-neutral-500 rounded-md p-7">
-            <h5>Case study</h5>
-            <h1>PSV — responsive website development</h1>
-          </div>
-        </a>
+        <div className="grid grid-cols-2 gap-4">
+          <a
+            className="mb-10 hover:scale-[1.02] transition ease-in-out border-solid border-2 border-neutral-200 rounded-lg"
+            href="/caseStudy"
+          >
+            <div className="h-[80vh] rounded-md p-7">
+              <div className="">
+                <h5>Case study</h5>
+                <h1>PSV — responsive website development</h1>
+              </div>
+              <img
+                className="absolute top-0 left-0 z-[-1] h-[80vh] w-[100%] object-cover object-bottom rounded-lg"
+                src={require("../assets/images/PSV Tile.jpg")}
+                alt="tile mockup"
+              />
+            </div>
+          </a>
+        </div>
       </section>
     </>
   );
