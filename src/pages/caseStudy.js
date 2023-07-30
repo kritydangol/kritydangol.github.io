e7d49abee8d5ea800db57c274faf054a70d8ec35
mediokrity <kritydangol1@gmail.com>
2023-07-30 17:55:15 +0545

    case study nav added

--- src/pages/caseStudy.js
+++ src/pages/caseStudy.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "../components/nav";
 
 const CaseStudy = () => {
   const mockups = [
@@ -20,35 +21,38 @@ const CaseStudy = () => {
     },
   ];
   return (
-    <section className="lg:mx-56 sm:mx-10 mx-5">
-      <div>
-        <h5>Case study</h5>
-        <h1>PSV — responsive website development</h1>
-        <h5>My role</h5>
-        <h4>
-          Creative Direction
-          <br />
-          UI / UX
-          <br />
-          Website Development
-          <br />
-          Deployment
-        </h4>
-        <h5>Client</h5>
-        <h4>Parth Student Venture</h4>
-        <h5>Year</h5>
-        <h4>2017</h4>
-      </div>
-      <div className="images">
-        {mockups.map((item) => (
-          <img
-            src={require(`../assets/images/${item.fileName}`)}
-            alt={item.alt}
-            className="mb-7 border border-neutral-300 rounded"
-          />
-        ))}
-      </div>
-    </section>
+    <>
+      <Nav />
+      <section className="lg:mx-56 sm:mx-10 mx-5">
+        <div>
+          <h5>Case study</h5>
+          <h1>PSV — responsive website development</h1>
+          <h5>My role</h5>
+          <h4>
+            Creative Direction
+            <br />
+            UI / UX
+            <br />
+            Website Development
+            <br />
+            Deployment
+          </h4>
+          <h5>Client</h5>
+          <h4>Parth Student Venture</h4>
+          <h5>Year</h5>
+          <h4>2017</h4>
+        </div>
+        <div className="images">
+          {mockups.map((item) => (
+            <img
+              src={require(`../assets/images/${item.fileName}`)}
+              alt={item.alt}
+              className="mb-7 border border-neutral-300 rounded"
+            />
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
