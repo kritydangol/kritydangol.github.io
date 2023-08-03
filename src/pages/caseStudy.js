3d81ba43928710d95176da3ebf9f46547c187f7a
mediokrity <kritydangol1@gmail.com>
2023-08-03 21:32:14 +0545

    structure updated

--- src/pages/caseStudy.js
+++ src/pages/caseStudy.js
@@ -24,27 +24,36 @@ const CaseStudy = () => {
     <>
       <Nav />
       <section className="lg:mx-56 sm:mx-10 mx-5 pb-2">
-        <div className="pt-[100px] mb-20">
-          <h5 className="subH pt-5">Case study</h5>
-          <h1 className="text-3xl">PSV — responsive website development</h1>
-          <h5 className="subH pt-5">My role</h5>
-          <h4>
-            Creative Direction
-            <br />
-            UI / UX
-            <br />
-            Website Development
-            <br />
-            Deployment
-          </h4>
-          <h5 className="subH pt-5">Client</h5>
-          <h4>
-            <a href="https://parthstudent.vc/" target="_blank" rel="noreferrer">
-              Parth Student Venture
-            </a>
-          </h4>
-          <h5 className="subH pt-5">Year</h5>
-          <h4>2022</h4>
+        <div className="pt-[100px] mb-20 grid grid-cols-2 gap-4">
+          <div>
+            <h5 className="subH pt-5">Case study</h5>
+            <h1 className="text-3xl">PSV — responsive website development</h1>
+            <h5 className="subH pt-5">My role</h5>
+            <h4>
+              Creative Direction
+              <br />
+              UI / UX
+              <br />
+              Website Development
+              <br />
+              Deployment
+            </h4>
+            <h5 className="subH pt-5">Client</h5>
+            <h4>
+              <a
+                href="https://parthstudent.vc/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Parth Student Venture
+              </a>
+            </h4>
+            <h5 className="subH pt-5">Year</h5>
+            <h4>2022</h4>
+          </div>
+          <p>
+            
+          </p>
         </div>
         <div className="images">
           {mockups.map((item) => (
