2cc635a78fbc157745946af88131c12bd5e35dba
mediokrity <kritydangol1@gmail.com>
2023-08-25 23:28:42 +0545

    categories updated

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Projects from "../components/projects";
+import Nav from "../components/nav";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div>
+      <Nav />
+      <Projects />
+    </div>
+  );
 };
 
 export default Categories;
