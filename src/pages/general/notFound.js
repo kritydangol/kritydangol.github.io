2e2cbf80ff2a5f6d8a582db5f01c685552c97e44
mediokrity <kritydangol1@gmail.com>
2023-09-22 17:15:05 +0545

    minor changes

--- src/pages/general/notFound.js
+++ src/pages/general/notFound.js
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h1 className="text-4xl">Are you lost, baby gorl?</h1>
+      <h1 className="text-4xl text-center">Are you lost, baby gorl?</h1>
     </div>
   );
 };
