6144ca3819fb4b7fb4a4567ef5411871801e5c06
mediokrity <kritydangol1@gmail.com>
2023-09-25 22:50:35 +0545

    nav added in not found,, minor changes

--- src/pages/general/notFound.js
+++ src/pages/general/notFound.js
@@ -1,19 +1,23 @@
 import React from "react";
+import Nav from "../../components/nav";
 
 const NotFound = () => {
   return (
-    <div className="text-center mt-[10vh]">
-      <img
-        className="mx-auto"
-        src={require("../../assets/images/404.png")}
-        alt="404"
-      />
-      <h1 className="text-4xl">Page Not Found</h1>
-      <p className="pt-3 pb-8">Are you lost, baby gorl?</p>
-      <a href="/#/home" className="link">
-        Go Home
-      </a>
-    </div>
+    <>
+      <Nav />
+      <div className="text-center mt-[10vh]">
+        <img
+          className="mx-auto"
+          src={require("../../assets/images/404.png")}
+          alt="404"
+        />
+        <h1 className="text-4xl">Page Not Found</h1>
+        <p className="pt-3 pb-8">Are you lost, baby gorl?</p>
+        <a href="/#/home" className="link">
+          Go Home
+        </a>
+      </div>
+    </>
   );
 };
 
