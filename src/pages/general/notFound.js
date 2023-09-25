9b17a341e23cf4e2fe60874c1a171ac27e38f77d
mediokrity <kritydangol1@gmail.com>
2023-09-25 22:43:14 +0545

    added 404 image

--- src/pages/general/notFound.js
+++ src/pages/general/notFound.js
@@ -3,7 +3,13 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center">Are you lost, baby gorl?</h1>
+      <img
+        className="mx-auto"
+        src={require("../../assets/images/404.png")}
+        alt="404"
+      />
+      <h1 className="text-4xl text-center">Page Not Found</h1>
+      <p className="text-center py-5">Are you lost, baby gorl?</p>
     </div>
   );
 };
