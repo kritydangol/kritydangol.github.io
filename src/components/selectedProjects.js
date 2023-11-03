076eb211ec28ce8dbcf5d83cf48db7c4df4deef5
kritydangol <kritydangol1@gmail.com>
2023-11-03 23:31:09 +0545

    selected projects added

---
+++ src/components/selectedProjects.js
@@ -0,0 +1,56 @@
+import React from "react";
+import useFetch from "../utils/useFetch";
+import { API_URL } from "../utils/constants";
+import Loader from "./loader";
+
+const SelectedProjects = () => {
+  const { data: projects, error } = useFetch(`${API_URL}portfolio.json`);
+  console.log(projects);
+  
+  return (
+    <section id="projects" className="lg:mx-56 sm:mx-10 mx-5">
+      <p className="subH">Selected Projects</p>
+      <h3 className="midH">Case Studies</h3>
+      {projects ? (
+        <div className="grid sm:grid-cols-2 gap-4 pb-10">
+          {/* mapping projects */}
+          {projects.map((project) => (
+            <a
+              key={project.id}
+              className="hover:scale-[1.02] transition ease-in-out border-solid border-2 border-neutral-200 rounded-lg"
+              href={`/#/projects/` + project.id}
+            >
+              <div className="md:h-[80vh] h-[60vh] rounded-md p-7">
+                <p className="subH mb-0">
+                  {project.tag[0]} {project.tag[1] && "| " + project.tag[1]}
+                </p>
+                <h1
+                  className={`smallH max-w-[380px] font-[700] capitalize ${
+                    project.tileColor === "black"
+                      ? "text-neutral-800"
+                      : "text-neutral-50"
+                  }`}
+                >
+                  {project.title}
+                </h1>
+                {project.tileImg && (
+                  <img
+                    className="absolute top-0 left-0 z-[-1] h-full w-[100%] object-cover object-bottom rounded-lg"
+                    src={require(`../assets/images/mockups/${project.id}/${project.tileImg}`)}
+                    alt={project.title}
+                  />
+                )}
+              </div>
+            </a>
+          ))}
+        </div>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <Loader />
+      )}
+    </section>
+  );
+};
+
+export default SelectedProjects;
