f4491f7f21270012e6d3237f8814f91236094250
mediokrity <kritydangol1@gmail.com>
2023-11-20 09:36:46 +0545

    trying out landing pages

--- src/components/home/landing.js
+++ src/components/home/landing.js
@@ -5,8 +5,22 @@ const Landing = () => {
   const currentYear = new Date().getFullYear();
   const totalYears = currentYear - 2021;
   return (
-    <section className="grid md:grid-cols-2 gap-4 lg:mx-56 sm:mx-10 mx-5 min-h-screen">
+    <section className="grid md:grid-cols-2 gap-4 lg:px-56 sm:px-10 px-5 min-h-screen bg-[#efebff]">
       <ScrollToAnchor />
+      <div className="md:table hidden">
+        <div className="table-cell align-middle">
+          <img
+            src={require("../../assets/images/avatar.png")}
+            alt="logo"
+            className="mx-auto"
+          />
+          {/* <img
+            src={require("../../assets/images/logo-circle-06.png")}
+            alt="logo"
+            className="imageRot mx-auto"
+          /> */}
+        </div>
+      </div>
       <div className="table">
         <div className="table-cell align-middle">
           <p className="subH">Krity Dangol</p>
@@ -24,15 +38,6 @@ const Landing = () => {
           <a href="/#/home" className="link">
             Download CV
           </a>
-        </div>
-      </div>
-      <div className="md:table hidden">
-        <div className="table-cell align-middle">
-          <img
-            src={require("../../assets/images/logo-circle-06.png")}
-            alt="logo"
-            className="imageRot mx-auto"
-          />
         </div>
       </div>
     </section>
