c906f585f6d9b5b78d5c4ddf85a00fb7cd17bade
mediokrity <kritydangol1@gmail.com>
2023-12-06 16:26:53 +0545

    collapsible sidebar in playground

--- src/pages/playground/playground.js
+++ src/pages/playground/playground.js
@@ -1,8 +1,20 @@
-import React from "react";
-import "./playground.css";
-
+// components/Playground.tsx
+import React, { useState } from "react";
 const Playground = () => {
-  return <div>Playground</div>;
+  const [collapsed, setSidebarCollapsed] = useState(false);
+  return (
+    <div
+      className={`grid min-h-screen ${
+        collapsed ? "grid-cols-sidebar" : "grid-cols-sidebar-collapsed"
+      } transition-[grid-template-columns] duration-300 ease-in-out`}
+    >
+      {/* sidebar */}
+      <div className="bg-indigo-700 text-white">
+        <button onClick={() => setSidebarCollapsed(!collapsed)}>close</button>
+      </div>
+      {/* content */}
+      <div className="">kjfnksejdn</div>
+    </div>
+  );
 };
-
 export default Playground;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        sidebar: "300px auto", //for sidebar layout
+        "sidebar-collapsed": "64px auto", //for collapsed sidebar layout
+      },
+    },
   },
   plugins: [],
 };
