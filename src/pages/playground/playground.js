1db763e0eede6039d9a958d664a5baed7ebb2913
mediokrity <kritydangol1@gmail.com>
2023-12-07 16:49:32 +0545

    added sidebar link

--- src/pages/playground/playground.js
+++ src/pages/playground/playground.js
@@ -9,6 +9,7 @@ const Playground = () => {
       } transition-[grid-template-columns] duration-300 ease-in-out`}
     >
       {/* sidebar */}
+      {/* https://reacthustle.com/blog/nextjs-react-responsive-collapsible-sidebar-tailwind#step-4-creating-the-sidebar-component */}
       <div className="bg-indigo-700 text-white">
         {!collapsed && <span className="whitespace-nowrap">My Logo</span>}
 
