cff2cd8c516db6d72b6192699783a567dda294dc
kritydangol <kritydangol1@gmail.com>
2024-05-21 22:45:29 +0545

    🤏🔄️

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "../components/nav";
 import Landing from "../components/home/landing";
 import Projects from "../components/projects";
-import Categories from "../components/home/categories";
+// import Categories from "../components/home/categories";
 import SelectedProjects from "../components/selectedProjects";
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-2xl md:px-10 px-5 mx-auto">
         <SelectedProjects />
         <Projects />
-        <Categories />
+        {/* <Categories /> */}
       </div>
     </>
   );
